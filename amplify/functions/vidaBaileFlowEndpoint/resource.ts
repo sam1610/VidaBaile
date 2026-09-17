4bff6f794497a0943904c93c375c44ea33f1f17b
sam1610 <samir.amri@gmail.com>
2026-09-17 11:58:50 +0300

    update  the lambda functioin maanging Flow

--- amplify/functions/vidaBaileFlowEndpoint/resource.ts
+++ amplify/functions/vidaBaileFlowEndpoint/resource.ts
@@ -1,8 +1,13 @@
-import { defineFunction } from '@aws-amplify/backend';
+import { defineFunction,secret } from '@aws-amplify/backend';
 
 export const vidaBaileFlowEndpoint = defineFunction({
   name: 'vidaBaileFlowEndpoint',
   entry: './handler.ts',
   timeoutSeconds: 15,
+  environment: {
+    // This injects the secure parameter into process.env.FLOW_PRIVATE_KEY
+    FLOW_PRIVATE_KEY: secret('FLOW_PRIVATE_KEY'),
+    // Note: TABLE_NAME will be passed dynamically from backend.ts
+  },
   // We will need to configure Function URL mapping in backend.ts
 });
